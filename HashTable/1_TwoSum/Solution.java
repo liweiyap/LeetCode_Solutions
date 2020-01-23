a91c37e5a22afb544b66fc4231e88d4205979d57
liweiyap <liweiyap@gmail.com>
2020-01-23 18:31:16 +0100

    Add code for Two Sum

---
+++ HashTable/1_TwoSum/Solution.java
@@ -0,0 +1,26 @@
+// Your memory usage beats 99.61 % of java submissions.
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        int output[] = new int[2];
+        for (int idx = 0; idx < nums.length; ++idx)
+        {
+            int diff = target - nums[idx];
+            for (Map.Entry<Integer, Integer> entry : hash.entrySet())
+            {
+                if (entry.getValue() == nums[idx])
+                {
+                    output[0] = entry.getKey();
+                    output[1] = idx;
+                    return output;
+                }
+            }
+            hash.put(idx, diff);
+        }
+        return output;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Solutions on [LeetCode](https://leetcode.com/) website
+
+This repository contains my solutions to specific coding challenges on the LeetCode website, as part of my preparation for technical job interviews. All challenges have been classified according to their respective topics. For each of these challenges, I have uploaded only the parts of the code that required completion, so the `public static void main (String[] args)` function is absent.
