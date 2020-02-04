2efddc289d98ffff102238bd2cf016e107debe27
liweiyap <liweiyap@gmail.com>
2020-02-04 15:23:09 +0100

    Add code for Binary Tree Inorder Traversal

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 This repository contains my solutions to specific coding challenges on the LeetCode website, as part of my preparation for technical job interviews. All challenges have been classified according to their respective topics. For each of these challenges, I have uploaded only the parts of the code that required completion, so the `main()` function is absent.
 
-Total number of questions solved: `19`
+Total number of questions solved: `20`
 
 Level of difficulty of questions solved:
 * Easy: `3`
-* Medium: `15`
+* Medium: `16`
 * Hard: `1`
 
 Language used:
 * Java: `19`
+* C++: `1`
---
+++ Tree/0094_BinaryTreeInorderTraversal/Solution.cpp
@@ -0,0 +1,60 @@
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Binary Tree Inorder Traversal.
+// Memory Usage: 9.2 MB, less than 89.00% of C++ online submissions for Binary Tree Inorder Traversal.
+
+#include <vector>
+#include <stack>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        std::vector<int> inorderVals;
+        if (!root)
+        {
+            return inorderVals;
+        }
+        
+        std::vector<TreeNode*> inorderNodes;
+        std::stack<TreeNode*> s;
+        s.push(root);
+        TreeNode* node;
+        
+        while (!s.empty())
+        {
+            node = s.top();
+            if (node->left &&
+                std::find(inorderNodes.begin(), inorderNodes.end(), node->left) == inorderNodes.end())
+            {
+                s.push(node->left);
+            }
+            else
+            {
+                s.pop();
+                inorderNodes.push_back(node);
+                
+                if (node->right &&
+                std::find(inorderNodes.begin(), inorderNodes.end(), node->right) == inorderNodes.end())
+                {
+                    s.push(node->right);
+                }
+            }
+        }
+        
+        for (TreeNode* node : inorderNodes)
+        {
+            inorderVals.push_back(node->val);
+        }
+        
+        return inorderVals;
+    }
+};
