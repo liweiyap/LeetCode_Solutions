6eaae8ef0349f80d440854e77d42c4292314f557
liweiyap <liweiyap@gmail.com>
2020-02-05 22:40:13 +0100

    Add code for Daily Temperatures

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This repository contains my solutions to specific coding challenges on the LeetCode website, as part of my preparation for technical job interviews. All challenges have been classified according to their respective topics. For each of these challenges, I have uploaded only the parts of the code that required completion, so the `main()` function is absent.
 
-Total number of questions solved: `22`
+Total number of questions solved: `23`
 
 Level of difficulty of questions solved:
 * Easy: `3`
-* Medium: `17`
+* Medium: `18`
 * Hard: `2`
 
 Language used:
 * Java: `19`
-* C++: `3`
+* C++: `4`
---
+++ Stack/0739_DailyTemperatures/Solution.cpp
@@ -0,0 +1,28 @@
+// Runtime: 176 ms, faster than 97.94% of C++ online submissions for Daily Temperatures.
+// Memory Usage: 16.2 MB, less than 64.00% of C++ online submissions for Daily Temperatures.
+
+#include <vector>
+#include <stack>
+
+class Solution
+{
+public:
+    std::vector<int> dailyTemperatures(std::vector<int>& T)
+    {
+        std::vector<int> waitingTimes(T.size(), 0);
+        std::stack<int> s;
+        
+        for (int currIdx = 0; currIdx < T.size(); ++currIdx)
+        {
+            while (!s.empty() && T[currIdx] > T[s.top()])
+            {
+                waitingTimes[s.top()] = currIdx - s.top();
+                s.pop();
+            }
+            
+            s.push(currIdx);
+        }
+        
+        return waitingTimes;
+    }
+};
