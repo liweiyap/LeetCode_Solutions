960c87a13e80458d6ac5227b5c0169e5ec20649b
liweiyap <liweiyap@gmail.com>
2020-04-02 21:50:18 +0100

    Add code for Reformat Department Table

---
+++ Database/1179_ReformatDepartmentTable/Solution.sql
@@ -0,0 +1,18 @@
+# Runtime: 583 ms, faster than 41.75% of MySQL online submissions for Reformat Department Table.
+# Memory Usage: 0B, less than 100.00% of MySQL online submissions for Reformat Department Table.
+
+SELECT id,
+       SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+       SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+       SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+       SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+       SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+       SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+       SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+       SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+       SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+       SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+       SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+       SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
