0061e33b19034044b3508422968a9dbdffccb6a1
liweiyap <liweiyap@gmail.com>
2021-04-18 13:05:19 +0800

    Add code for LRU Cache

---
+++ Design/0146_LRUCache/Solution.java
@@ -0,0 +1,152 @@
+/* Without LinkedHashMap */
+
+// Runtime: 251 ms, faster than 5.03% of Java online submissions for LRU Cache.
+// Memory Usage: 47.3 MB, less than 41.96% of Java online submissions for LRU Cache.
+
+class LRUCache
+{
+    public LRUCache(int capacity)
+    {
+        _capacity = capacity;
+    }
+
+    public int get(int key)
+    {
+        if (!_hashMap.containsKey(key))
+        {
+            return -1;
+        }
+
+        Iterator _keyLinkedListIt = _keyLinkedList.iterator();
+        while (_keyLinkedListIt.hasNext())
+        {
+            int currKey = (int) _keyLinkedListIt.next();
+
+            if (currKey == key)
+            {
+                _keyLinkedListIt.remove();
+                _keyLinkedList.addLast(key);
+                break;
+            }
+        }
+
+        return (int) _hashMap.get(key);
+    }
+
+    public void put(int key, int value)
+    {
+        Iterator _keyLinkedListIt = _keyLinkedList.iterator();
+        while (_keyLinkedListIt.hasNext())
+        {
+            int currKey = (int) _keyLinkedListIt.next();
+
+            if (currKey == key)
+            {
+                _keyLinkedListIt.remove();
+                _hashMap.remove(key);
+                break;
+            }
+        }
+
+        if (_capacity == _keyLinkedList.size())
+        {
+            int firstKey = (int) _keyLinkedList.iterator().next();
+            System.out.print("put 0. firstKey = " + firstKey + "\n======\n");
+            _keyLinkedList.removeFirst();
+            _hashMap.remove(firstKey);
+        }
+
+        _keyLinkedList.addLast(key);
+        _hashMap.put(key, value);
+    }
+
+    private int _capacity;
+    private LinkedList _keyLinkedList = new LinkedList<Integer>();
+    private HashMap _hashMap = new HashMap<Integer,Integer>();
+}
+
+/* Long version of LinkedHashMap */
+
+// Runtime: 15 ms, faster than 30.47% of Java online submissions for LRU Cache.
+// Memory Usage: 46.6 MB, less than 97.39% of Java online submissions for LRU Cache.
+
+// class LRUCache
+// {
+//     public LRUCache(int capacity)
+//     {
+//         _capacity = capacity;
+//         _linkedHashMap = new LinkedHashMap<Integer,Integer>(capacity);
+//     }
+
+//     public int get(int key)
+//     {
+//         if (!_linkedHashMap.containsKey(key))
+//         {
+//             return -1;
+//         }
+
+//         Object value = _linkedHashMap.get(key);
+//         _linkedHashMap.remove(key);      // remove ...
+//         _linkedHashMap.put(key, value);  // and add back
+
+//         return (int)value;
+//     }
+
+//     public void put(int key, int value)
+//     {
+//         if (get(key) != -1)
+//         {
+//             _linkedHashMap.remove(key);
+//         }
+
+//         if (_linkedHashMap.size() == _capacity)
+//         {
+//             Object firstKey = _linkedHashMap.keySet().iterator().next();
+//             _linkedHashMap.remove(firstKey);
+//         }
+
+//         _linkedHashMap.put(key, value);
+//     }
+
+//     private int _capacity;
+//     private LinkedHashMap _linkedHashMap;
+// }
+
+/* Short version of LinkedHashMap */
+
+// Runtime: 18 ms, faster than 20.61% of Java online submissions for LRU Cache.
+// Memory Usage: 57.1 MB, less than 5.02% of Java online submissions for LRU Cache.
+
+// class LRUCache
+// {
+//     public LRUCache(int capacity)
+//     {
+//         _linkedHashMap = new LinkedHashMap<Integer,Integer>(capacity, 0.75F, true)
+//         {
+//             protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest)
+//             {
+// 				return size() > capacity;
+// 			}
+// 		};
+//     }
+
+//     public int get(int key)
+//     {
+//         return (int) _linkedHashMap.getOrDefault(key, -1);
+//     }
+
+//     public void put(int key, int value)
+//     {
+//         _linkedHashMap.put(key, value);
+//     }
+
+//     private LinkedHashMap _linkedHashMap;
+// }
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 This repository contains my solutions to specific coding challenges on the LeetCode website, as part of my preparation for technical job interviews. All challenges have been classified according to their respective topics. For each of these challenges, I have uploaded only the parts of the code that required completion, so the `main()` function is absent.
 
-Total number of questions solved: `89`
+Total number of questions solved: `90`
 
 Level of difficulty of questions solved:
 * Easy: `29` (excluding SQL: `17`)
-* Medium: `51` (excluding SQL: `46`)
+* Medium: `52` (excluding SQL: `47`)
 * Hard: `9` (excluding SQL: `6`)
 
 Language used (some questions are solved multiple times using different languages):
 * C++: `42`
-* Java: `25`
+* Java: `26`
 * SQL: `20`
 * Bash: `3`
 * C: `3`
